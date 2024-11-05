160123d78ad368de53d0366f574f2b87a9af563f
test <test@test.com>
2024-11-05 19:19:13 +0900

    SNS 글 목록

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,16 +1,27 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
 
 @Controller
 public class TimelineController {
 
-	//@Autowried
-	//private PostBO postBO;
+	@Autowired
+	private PostBO postBO;
 	
 	@GetMapping("/timeline")
-	public String timeline() {
+	public String timeline(Model model) {
+		List<PostEntity> postList = postBO.getPostList();	
+		
+		model.addAttribute("postList", postList);
+			
 		return "timeline/timeline";
 	}
 }
